a124a536ebedd0501111a7c4b70f4f014f802a5d
ZhivkoAndreev <zhia@nuuday.dk>
2023-06-04 22:46:43 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import { useState, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/Global.scss";
-import "../styles/Trends.scss";
 import "../styles/Navigation.scss";
 import "../styles/VacationPanel.scss";
-import "../styles/RandomCity.scss";
 import "../styles/ActionBar.scss";
 import "../styles/ErrorUI.scss";
 import "../styles/Footer.scss";
 import "../styles/Room.scss";
 
 import Layout from "./components/ui/Layout";
-// import Admin from "./components/Admin";
 
 // Lazy loading - the components will load only when the user visits them
 
@@ -22,11 +19,7 @@ import Layout from "./components/ui/Layout";
 // const Login = lazy(() => import("./components/ui/LoginPanel"));
 // const Register = lazy(() => import("./components/ui/RegisterPanel"));
 // const UpdateProduct = lazy(() => import("./components/UpdateProduct"));
-// const TrendsCurrency = lazy(() => import("./components/TrendsCurrency"));
-// const RandomCity = lazy(() => import("./components/RandomCity"));
-// const Testing = lazy(() => import("./components/Testing"));
-// const Slider = lazy(() => import("./components/Slider"));
-// const Google = lazy(() => import("./components/Google"));
+
 const RoomGrid = lazy(() => import("./components/RoomGrid"));
 
 function App() {
@@ -50,13 +43,8 @@ function App() {
               {/* <Route path="/shop" element={<Shop />} />
               <Route path="/update-product/:id" element={<UpdateProduct />} />
               <Route path="/vacations" element={<Vacations />} />
-              <Route path="/trends" element={<TrendsCurrency />} />
-              <Route path="/random-city" element={<RandomCity />} />
-              <Route path="/slider" element={<Slider />} />
-              <Route path="/testing" element={<Testing />} />
               <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/google" element={<Google />} /> */}
+              <Route path="/register" element={<Register />} /> */}
             </Routes>
           </Layout>
         </Suspense>
--- src/components/RoomGrid.tsx
+++ src/components/RoomGrid.tsx
@@ -2,9 +2,18 @@ import Room from "./ui/Room";
 import { fetchRooms } from "../utils/API";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import LoadingSpinners from "./ui/LoadingSpinners";
+import ErrorUI from "./ui/ErrorUI";
 
 const RoomGrid = () => {
   const { data, isLoading, isError } = useQuery(["rooms"], fetchRooms);
+
+  if (isLoading) {
+    return <LoadingSpinners magnifying_glass />;
+  }
+  if (isError) {
+    return <ErrorUI />;
+  }
 
   return (
     <div className="rooms">
